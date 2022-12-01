9ec23068e9381cba0e6d0db514d8456edefa2a6c
PowerPii <errorfucntion@gmail.com>
2022-12-01 13:21:23 +0800

    Edited Version for PowerPii Bot

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,6 @@
 from telegram.ext import *
 from datetime import date
+from dateutil.relativedelta import *
 
 API_KEY = "5949204184:AAE26em2OpaRD_44tUfsNZCHFooBZCJbWok"
 admin_id = "602622095"
@@ -9,7 +10,7 @@ def start(update, context):
     user_id = str(update.message.from_user.id)
 
     if user_id == admin_id:
-        day_count_text = "<code>YOu know what this bot does, Fifa. XOXO</code>"
+        day_count_text = "<code>You know what this bot does, Fifa. XOXO</code>"
 
         context.bot.send_message(text=day_count_text, chat_id=user_id, parse_mode="HTML")
 
@@ -20,9 +21,15 @@ def stats(update, context):
     if user_id == admin_id:
         d0 = date(2022, 11, 23)
         d1 = date.today()
-        difference = d1 - d0
+        delta = relativedelta(d1, d0)
 
-        day_count_text = f"<code>It's been {difference.days} days!</code>"
+        day_count_text = f"<code>It's been {delta.days} day(s)!</code>"
+
+        if delta.years == 0 and delta.months > 0:
+            day_count_text = f"<code>It's been {delta.months} month(s) and {delta.days} day(s)!</code>"
+        if delta.years > 0 and delta.months > 0:
+            day_count_text = f"<code>It's been {delta.years} year(s) {delta.months} month(s) and {delta.days} day(" \
+                             f"s)!</code> "
 
         context.bot.send_message(text=day_count_text, chat_id=user_id, parse_mode="HTML")
 
@@ -48,9 +55,3 @@ if __name__ == "__main__":
     print("Bot started...")
 
     main()
-
-
-
-
-
-
